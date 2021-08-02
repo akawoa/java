5fe0d3f05b9704fd2bb0528190dcdcb0d1a67195
akawoa <72364753+akawoa@users.noreply.github.com>
2021-08-02 09:30:53 -0700

    added assignment

---
+++ java_fundamentals/Your_Own_Java_Program/YourOwnJavaProgram.java
@@ -0,0 +1,7 @@
+public class YourOwnJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Ryne Rountree");
+        System.out.println("I am 29 years old");
+        System.out.println("My hometown is Boise, Idaho");
+    }
+}
