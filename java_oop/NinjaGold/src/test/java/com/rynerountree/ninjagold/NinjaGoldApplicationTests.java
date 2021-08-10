f42f5fb74b47196c02368ba428679476708b7838
akawoa <72364753+akawoa@users.noreply.github.com>
2021-08-10 08:22:00 -0700

    added assignments

---
+++ java_oop/NinjaGold/src/test/java/com/rynerountree/ninjagold/NinjaGoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rynerountree.ninjagold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaGoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
